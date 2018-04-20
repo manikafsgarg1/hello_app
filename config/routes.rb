848a4017889c1f649125dc8b4cd7278478e73190
manikafsgarg1 <manikafsgarg@gmail.com>
2018-04-20 11:04:04 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	# root 'controller_name#action_name'   |||Format|||
-	# root 'application#hello'
-	root 'application#goodbye'
+	root 'application#hello'
+	# root 'application#goodbye'
 end
